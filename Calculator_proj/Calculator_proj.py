4bad84ab3527b3e371d0d4aa5918dbfc608bdf56
tomyg151 <77575527+tomyg151@users.noreply.github.com>
2022-12-03 18:07:58 +0200

    Add files via upload

---
+++ Calculator_proj/Calculator_proj.py
@@ -0,0 +1,50 @@
+from replit import clear
+from art import logo
+
+#Add
+def add(n1, n2):
+    return n1 + n2
+
+#subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+#multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+#divide
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+def calculator():
+    """loop that calculate the user input while his still want to run the function if the user type 'n' as 'no'
+       so the calculator go back to the start position in a recursion"""
+    print(logo)
+    num1 = float(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+            clear()
+            calculator()
+
+calculator()
